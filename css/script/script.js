9eb9a169c3842ba1eba7aea69df2fcc0898e7fa3
lucasglima-wep <lucasglimasousa12@gmail.com>
2026-03-08 15:25:07 -0300

    fazendo a dela de inicio

---
+++ css/script/script.js
@@ -0,0 +1,8 @@
+const toggle = document.getElementById("toggle");
+const sidebar = document.getElementById("sidebar");
+
+toggle.addEventListener("click", function () {
+
+sidebar.classList.toggle("close");
+
+});
